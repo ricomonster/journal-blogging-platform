ba1f51e5c6103e7df5830ba23d41cb6b2ea526fb
ricomonster <rmaglayon@gmail.com>
2015-12-20 01:12:59 +0800

    routes are now authenticated. added logout functionality

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -24,7 +24,7 @@
     angular.module('journal.config', ['LocalStorageModule', 'toastr']);
     angular.module('journal.constants', []);
     angular.module('journal.routes', ['ui.router', 'journal.constants']);
-    angular.module('journal.run', []);
+    angular.module('journal.run', ['ngProgressLite']);
 
     // COMPONENTS
     // Editor
@@ -157,7 +157,35 @@
 (function() {
     'use strict';
 
-    angular.module('journal.run');
+    angular.module('journal.run')
+        .run(['$rootScope', '$state', '$timeout', 'AuthService', 'ngProgressLite', AuthenticateRoutes]);
+
+    function AuthenticateRoutes($rootScope, $state, $timeout, AuthService, ngProgressLite) {
+        $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+            // start ngprogress
+            ngProgressLite.start();
+
+            // check if the next route needs to be authenticated
+            if (toState.authenticate && !AuthService.user()) {
+                // redirect to login page
+                $state.transitionTo('login');
+                event.preventDefault();
+            }
+
+            // check if the next page is the login page
+            if (toState.name == 'login' && AuthService.user()) {
+                // user is logged in, redirect to post lists
+                $state.transitionTo('post.lists');
+                event.preventDefault();
+            }
+        });
+
+        $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+            $timeout(function() {
+                ngProgressLite.done();
+            });
+        });
+    }
 })();
 (function() {
     'use strict';
--- resources/src/app.run.js
+++ resources/src/app.run.js
@@ -1,5 +1,33 @@
 (function() {
     'use strict';
 
-    angular.module('journal.run');
+    angular.module('journal.run')
+        .run(['$rootScope', '$state', '$timeout', 'AuthService', 'ngProgressLite', AuthenticateRoutes]);
+
+    function AuthenticateRoutes($rootScope, $state, $timeout, AuthService, ngProgressLite) {
+        $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+            // start ngprogress
+            ngProgressLite.start();
+
+            // check if the next route needs to be authenticated
+            if (toState.authenticate && !AuthService.user()) {
+                // redirect to login page
+                $state.transitionTo('login');
+                event.preventDefault();
+            }
+
+            // check if the next page is the login page
+            if (toState.name == 'login' && AuthService.user()) {
+                // user is logged in, redirect to post lists
+                $state.transitionTo('post.lists');
+                event.preventDefault();
+            }
+        });
+
+        $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+            $timeout(function() {
+                ngProgressLite.done();
+            });
+        });
+    }
 })();
