e5b8869d6c7f8199dd2db6e86a6092f4a0ebcf5d
ricomonster <rmaglayon@gmail.com>
2016-01-02 14:54:53 +0800

    added a directive to set the text if the post is published or to be published in the near future. change the font from Open Sans to Roboto.

--- public/assets/js/directives.js
+++ public/assets/js/directives.js
@@ -363,6 +363,39 @@
 (function() {
     'use strict';
 
+    angular.module('journal.components.postLists')
+        .directive('publishIndicator', [PublishIndicatorDirective]);
+
+    function PublishIndicatorDirective() {
+        return {
+            restrict : 'A',
+            scope : {
+                publishIndicator : '=publishIndicator'
+            },
+            link : function(scope, element, attribute) {
+                var currentTimestamp = Math.floor(Date.now() / 1000);
+
+                if (attribute.publishIndicator) {
+                    scope.$watch(function() {
+                        return scope.publishIndicator
+                    }, function(timestamp) {
+                        // check if the post timestamp is greater than the
+                        // current timestamp then set the text to be shown.
+                        var text = (currentTimestamp >= timestamp) ?
+                            'Published' : 'To be published';
+
+                        // set the text
+                        element.text(text);
+                    });
+                }
+            }
+        }
+    }
+})();
+
+(function() {
+    'use strict';
+
     angular.module('journal.components.sidebar')
         .directive('journalSidebar', [SidebarDirective]);
 
