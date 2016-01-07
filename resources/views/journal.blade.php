25e41a09e59ac4eca2b2d8ab4eb6ed5563be3d2d
ricomonster <rmaglayon@gmail.com>
2016-01-07 15:28:04 +0800

    setup the production files

--- resources/views/journal.blade.php
+++ resources/views/journal.blade.php
@@ -20,31 +20,8 @@
     <!-- endinject -->
 
     <!-- inject:js -->
-    <script type="text/javascript" src="{{ asset('/vendor/js/codemirror.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/vendor/js/showdown.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/vendor/js/moment.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/vendor/js/spin.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/vendor/js/ladda.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/vendor/js/angular.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/vendor/js/angular-animate.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/vendor/js/angular-sanitize.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/vendor/js/angular-ui-router.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/vendor/js/ui-bootstrap.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/vendor/js/ui-bootstrap-tpls.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/vendor/js/ui-codemirror.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/vendor/js/angular-ladda.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/vendor/js/angular-local-storage.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/vendor/js/angular-toastr.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/vendor/js/angular-toastr.tpls.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/vendor/js/angular-moment.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/vendor/js/ng-file-upload-shim.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/vendor/js/ng-file-upload.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/vendor/js/ngprogress-lite.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/assets/js/app.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/assets/js/controllers.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/assets/js/directives.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/assets/js/providers.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/assets/js/services.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/vendor/vendor.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/vendor/journal.js') }}"></script>
     <!-- endinject -->
 </head>
 
