7e39136d53cae10d0ed4e1a1a05710d8d954c7cc
ricomonster <rmaglayon@gmail.com>
2016-01-09 23:03:02 +0800

    used spin.js for the loader.

--- resources/src/shared/journal-loader/JournalLoaderDirective.js
+++ resources/src/shared/journal-loader/JournalLoaderDirective.js
@@ -7,7 +7,19 @@
     function JournalLoaderDirective(CONFIG) {
         return {
             restrict : 'EA',
-            templateUrl : CONFIG.TEMPLATE_PATH + '/journal-loader/_journal-loader.html'
+            replace : true,
+            templateUrl : CONFIG.TEMPLATE_PATH + '/journal-loader/_journal-loader.html',
+            link : function(scope, element, attribute) {
+                // TODO: Dynamic settings to be put as an element attribute
+                var options = {
+                        color: '#4d4a4c',
+                        width: 4
+                    };
+
+                var spinner = new Spinner(options).spin()
+
+                element[0].appendChild(spinner.el);
+            }
         }
     }
 })();
