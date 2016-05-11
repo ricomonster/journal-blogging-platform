982f83aa45b2928c9d13909710eb8254acd30021
Rico Maglayon <rmaglayon@gmail.com>
2016-05-11 21:01:39 +0800

    fixed database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
         $this->call(RoleTableSeeder::class);
-=======
-         $this->call(RoleSeeder::class);
->>>>>>> 55ef9cdd20e1f3f8a49d2e17e257a0d5eadd821a
     }
 }
